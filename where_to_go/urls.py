d98b22f823d0f72dd22de23e09d110f842d2728f
Dmitriy877 <dmitriybukin05@gmail.com>
2025-07-17 21:49:40 +0300

    Setup media files serving
    Setup media files serving

--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
 from django.urls import path
 from where_to_go import views
 
@@ -22,4 +24,4 @@ from where_to_go import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.start_page)
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
