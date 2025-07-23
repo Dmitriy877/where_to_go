03f7b161231331b3761c34fb966dba02151c4a4b
Dmitriy877 <dmitriybukin05@gmail.com>
2025-07-23 22:26:29 +0300

    Delete test data and add  endpoint API
    Delete test data and add  endpoint API

--- places/views.py
+++ places/views.py
@@ -1,33 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
+from django.http import JsonResponse
 from django.template import loader
 from .models import Place
-import os
-import pprint
+from django.urls import reverse
 
 
 def start_page(request):
     places = Place.objects.all()
     places_for_index = []
+
     for place in places:
-        pictures = place.pictures.all()
-        pictures_list = [picture.picture.url for picture in pictures]
-
-        details_url = {
-            "title": place.title,
-            "imgs": pictures_list,
-            "description_short": place.description_short,
-            "description_long": place.description_long,
-            "coordinates": {
-                "lng": place.coordinates_lng,
-                "lat": place.coordinates_lat
-            }
-        }
-
-        if place.title == 'Экскурсионная компания «Легенды Москвы»':
-            path = os.path.join('static', 'places', 'moscow_legends.json')
-        else:
-            path = os.path.join('static', 'places', 'roofs24.json')
 
         place_for_index = {
             "type": "Feature",
@@ -38,7 +21,7 @@ def start_page(request):
             "properties": {
                 "title": place.title,
                 "placeId": place.place_id,
-                "detailsUrl": path
+                "detailsUrl": reverse(place_page, args=[place.id])
             }
         }
 
@@ -55,35 +38,29 @@ def start_page(request):
     return HttpResponse(rendered_page)
 
 
-# # Create your views here.
-# <script id="places-geojson" type="application/json">
-#   {
-#     "type": "FeatureCollection",
-#     "features": [
-#       {
-#         "type": "Feature",
-#         "geometry": {
-#           "type": "Point",
-#           "coordinates": [37.62, 55.793676]
-#         },
-#         "properties": {
-#           "title": "«Легенды Москвы",
-#           "placeId": "moscow_legends",
-#           "detailsUrl": "{% static "./places/moscow_legends.json" %}"
-#         }
-#       },
-#       {
-#         "type": "Feature",
-#         "geometry": {
-#           "type": "Point",
-#           "coordinates": [37.64, 55.753676]
-#         },
-#         "properties": {
-#           "title": "Крыши24.рф",
-#           "placeId": "roofs24",
-#           "detailsUrl": "{% static "./places/roofs24.json" %}"
-#         }
-#       }
-#     ]
-#   }
-# </script>
+def place_page(request, id: int):
+
+    place = get_object_or_404(Place, pk=id)
+
+    pictures = place.pictures.all()
+    pictures_urls = [
+        picture.picture.url for picture in pictures
+    ]
+
+    json_place = {
+        "title": place.title,
+        "imgs": pictures_urls,
+        "description_short": place.description_short,
+        "description_long": place.description_long,
+        "coordinates": {
+            "lng": place.coordinates_lng,
+            "lat": place.coordinates_lat
+        }
+    }
+
+    response = JsonResponse(json_place, json_dumps_params={
+                                        'indent': 2,
+                                        'ensure_ascii': False,
+                                     })
+
+    return response
--- where_to_go/urls.py
+++ where_to_go/urls.py
@@ -23,5 +23,6 @@ from places import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.start_page)
+    path('', views.start_page),
+    path('places/<int:id>/', views.place_page)
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
