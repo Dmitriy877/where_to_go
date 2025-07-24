4a20856c07a7910e237540b0558f2ffd39e07b0f
Dmitriy877 <dmitriybukin05@gmail.com>
2025-07-24 21:22:14 +0300

    Add preview for images in all models
    Add preview for images in all models

--- places/admin.py
+++ places/admin.py
@@ -1,17 +1,42 @@
 from django.contrib import admin
 from places.models import Place, Place_picture
+from django.utils.html import format_html
 
 
 class Place_pictureInLine(admin.TabularInline):
     model = Place_picture
+    readonly_fields = [
+            'get_picture_image',
+        ]
+
+    def get_picture_image(self, obj):
+        return format_html(
+            f'<img src="{obj.picture.url}" width="200" height=200 />'
+        )
+
+
+@admin.register(Place_picture)
+class Place_PictureAdmin(admin.ModelAdmin):
+    try:
+        readonly_fields = [
+            'get_picture_image',
+        ]
+
+        def get_picture_image(self, obj):
+            return format_html(
+                f'<img src="{obj.picture.url}" width="200" height=200 />'
+            )
+    except Exception as err:
+        print(Exception, err)
 
 
 @admin.register(Place)
 class PlaceAdmin(admin.ModelAdmin):
-    inlines = [
-        Place_pictureInLine
-    ]
+    try:
+        inlines = [
+            Place_pictureInLine
+        ]
+    except Exception as err:
+        print(Exception, err)
 
-
-admin.site.register(Place_picture)
 # Register your models here.
