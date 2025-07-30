7889877e6d6be25bbd59777a9d20d9aa627ca84f
Dmitriy877 <dmitriybukin05@gmail.com>
2025-07-30 22:44:27 +0300

    Add exceptions

--- places/management/commands/json_load.py
+++ places/management/commands/json_load.py
@@ -2,6 +2,8 @@ from django.core.management.base import BaseCommand
 from places.models import Place, PlacePicture
 from django.core.files.base import ContentFile
 import requests
+from requests.exceptions import HTTPError, ConnectionError
+import time
 
 
 class Command(BaseCommand):
@@ -34,15 +36,27 @@ class Command(BaseCommand):
             print('Указанная локация уже добавлена')
 
         image_urls = payload['imgs']
+
         for index, image_url in enumerate(image_urls, start=1):
-            response = requests.get(image_url)
-            response.raise_for_status()
-            content = ContentFile(
-                response.content,
-                name=f'{place_location}{index}'
-            )
-            picture, created = PlacePicture.objects.get_or_create(
-                number=index,
-                place=place_location,
-                picture=content,
-            )
+            step = 0
+            while step <= len(image_urls):
+                try:
+                    response = requests.get(image_url)
+                    response.raise_for_status()
+                    content = ContentFile(
+                        response.content,
+                        name=f'{place_location}{index}'
+                    )
+                    picture, created = PlacePicture.objects.get_or_create(
+                        number=index,
+                        place=place_location,
+                        picture=content,
+                    )
+                    step += 1
+                except HTTPError:
+                    print(f'Ошибка загрузки изображения по адресу {image_url}. Проверьте правильность указания ссылок')
+                    continue
+                except ConnectionError:
+                    print('Ошибка соединения. Попытка установить соединение')
+                    time.sleep(10)
+                    continue
